c27c63f27f14f7c7d59fd257044c451c112ac1bb
techupvibin <vibinnath.c@gmail.com>
2025-12-14 22:43:14 -0800

    add code

--- terraform-app/backed.tf
+++ terraform-app/backed.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bookstore-terraform-state"
+    bucket = "bookstore-eks-terraform-state-vibin"
     key    = "app/terraform.tfstate"
     region = "us-east-2"
     encrypt      = true
