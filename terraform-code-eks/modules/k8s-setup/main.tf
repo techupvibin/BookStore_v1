9e98408b8d51eeb1286b5743be953c04cc3f40bb
techupvibin <vibinnath.c@gmail.com>
2025-12-16 00:25:37 -0800

    a

--- terraform-code-eks/modules/k8s-setup/main.tf
+++ terraform-code-eks/modules/k8s-setup/main.tf
@@ -1,167 +1,130 @@
 ############################
-# Kubernetes Provider
+# k8s-apps module (Ingress + Monitoring)
 ############################
+
+# Kubernetes provider
 provider "kubernetes" {
   host                   = var.k8s_cluster_endpoint
   cluster_ca_certificate = base64decode(var.k8s_cluster_ca)
   token                  = var.k8s_auth_token
 }
 
-############################
-# Namespace
-############################
-resource "kubernetes_namespace" "bookstore" {
-  metadata {
-    name = "bookstore"
+# Helm provider
+provider "helm" {
+  kubernetes {
+    host                   = var.k8s_cluster_endpoint
+    cluster_ca_certificate = base64decode(var.k8s_cluster_ca)
+    token                  = var.k8s_auth_token
   }
 }
 
 ############################
-# Frontend Deployment
+# Kubernetes Namespaces
 ############################
-resource "kubernetes_deployment" "frontend" {
+resource "kubernetes_namespace" "ingress" {
   metadata {
-    name      = "frontend"
-    namespace = kubernetes_namespace.bookstore.metadata[0].name
-    labels = {
-      app = "frontend"
-    }
+    name = "ingress-nginx"
   }
 
-  spec {
-    replicas = 2
+  depends_on = [var.cluster_ready]
+}
 
-    selector {
-      match_labels = {
-        app = "frontend"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "frontend"
-        }
-      }
-
-      spec {
-        container {
-          name  = "frontend"
-          image = var.frontend_image_url
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
   }
+
+  depends_on = [var.cluster_ready]
 }
 
 ############################
-# Frontend Service
+# Ingress NGINX Helm Release
 ############################
-resource "kubernetes_service" "frontend" {
-  metadata {
-    name      = "frontend-svc"
-    namespace = kubernetes_namespace.bookstore.metadata[0].name
-  }
+resource "helm_release" "nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.14.1"
+  namespace        = kubernetes_namespace.ingress.metadata[0].name
+  create_namespace = false
 
-  spec {
-    selector = {
-      app = "frontend"
-    }
+  values = [
+    <<-EOT
+    controller:
+      service:
+        type: LoadBalancer
+    EOT
+  ]
 
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "ClusterIP"
-  }
+  wait = true
+  depends_on = [kubernetes_namespace.ingress]
 }
 
 ############################
-# Backend Deployment
+# Prometheus Helm Release
 ############################
-resource "kubernetes_deployment" "backend" {
-  metadata {
-    name      = "backend"
-    namespace = kubernetes_namespace.bookstore.metadata[0].name
-    labels = {
-      app = "backend"
-    }
-  }
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus"
+  version          = "27.50.1"
+  namespace        = kubernetes_namespace.monitoring.metadata[0].name
+  create_namespace = false
 
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "backend"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "backend"
-        }
-      }
-
-      spec {
-        container {
-          name  = "backend"
-          image = var.backend_image_url
-          port {
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
+  wait = true
+  depends_on = [kubernetes_namespace.monitoring]
 }
 
 ############################
-# Backend Service
+# Grafana Helm Release
 ############################
-resource "kubernetes_service" "backend" {
-  metadata {
-    name      = "backend-svc"
-    namespace = kubernetes_namespace.bookstore.metadata[0].name
-  }
+resource "helm_release" "grafana" {
+  name             = "grafana"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana"
+  version          = "10.3.1"
+  namespace        = kubernetes_namespace.monitoring.metadata[0].name
+  create_namespace = false
 
-  spec {
-    selector = {
-      app = "backend"
-    }
+  values = [
+    <<-EOT
+    adminUser: admin
+    adminPassword: admin123
+    service:
+      type: LoadBalancer
+    EOT
+  ]
 
-    port {
-      port        = 8080
-      target_port = 8080
-    }
-
-    type = "ClusterIP"
-  }
+  wait = true
+  depends_on = [kubernetes_namespace.monitoring]
 }
 
 ############################
 # Outputs
 ############################
-output "frontend_deployment_name" {
-  value = kubernetes_deployment.frontend.metadata[0].name
+output "nginx_release_name" {
+  value = helm_release.nginx.name
 }
 
-output "backend_deployment_name" {
-  value = kubernetes_deployment.backend.metadata[0].name
+output "prometheus_release_name" {
+  value = helm_release.prometheus.name
 }
 
-output "frontend_service_name" {
-  value = kubernetes_service.frontend.metadata[0].name
+output "grafana_release_name" {
+  value = helm_release.grafana.name
 }
 
-output "backend_service_name" {
-  value = kubernetes_service.backend.metadata[0].name
+output "ingress_namespace" {
+  value = kubernetes_namespace.ingress.metadata[0].name
 }
+
+output "monitoring_namespace" {
+  value = kubernetes_namespace.monitoring.metadata[0].name
+}
+
+############################
+# Variables
+############################
 variable "k8s_cluster_endpoint" {
   description = "EKS cluster endpoint"
   type        = string
@@ -177,12 +140,8 @@ variable "k8s_auth_token" {
   type        = string
 }
 
-variable "frontend_image_url" {
-  description = "ECR image URL for frontend"
-  type        = string
-}
-
-variable "backend_image_url" {
-  description = "ECR image URL for backend"
-  type        = string
+variable "cluster_ready" {
+  description = "Set to true once EKS cluster is ready"
+  type        = bool
+  default     = true
 }
